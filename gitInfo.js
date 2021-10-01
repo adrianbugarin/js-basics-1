d3efb37647e3b4fbe9c10ff367f6eccd50ed0afc
Adrian_Bugarin <adrianbugarin3@gmail.com>
2021-10-01 09:01:32 -0600

    initial commit

---
+++ gitInfo.js
@@ -0,0 +1,79 @@
+/*
+    For this section of the assessment you will be putting together a cheat sheet for common git commands.  
+    You'll provide the command as well as what it does. 
+*/
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Create a variable called 'gitDefinition'.
+    It should be a string containing your best definition of what Git is.
+*/
+
+//CODE HERE
+var gitDefinition =("git is a program; that tracks and records changes being made to a code")
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable called 'gitHubDefinition'.  
+    It should be a string containing your best definition of what GitHub is.
+*/
+
+//CODE HERE
+var gitHubDefinition = ("GitHub is a website; that stores any code on to the cloud service it provides, and is way to share and/or collaborate with your code.")
+//////////////////PROBLEMS 3 - 8////////////////////
+/*
+    For the next several problems you will be creating objects containing information about different git 
+    commands.  Each object should contain 'description' and 'code' properties.  The 'description' property 
+    will be a string with a description of what that git command does.  The 'code' property should be a 
+    string of the actual command (what you type into your terminal).
+*/
+
+//////////////////PROBLEM 3////////////////////
+/*
+    Create an object called 'init' with 'description' and 'code' properties 
+    following the guidelines above to describe the init command.
+*/
+
+//CODE HERE
+const init = {description:"initialize a local repository right here in this folder", code:"git init"};
+//////////////////PROBLEM 4////////////////////
+/*
+    Create an object called 'clone' with 'description' and 'code' properties 
+    following the guidelines above to describe the clone command.
+*/
+
+//CODE HERE
+const clone = {description:"get a copy of an existing Git repository", code:"git clone <url>"};
+//////////////////PROBLEM 5////////////////////
+/*
+    Create an object called 'status' with 'description' and 'code' properties 
+    following the guidelines above to describe the status command.
+*/
+
+//CODE HERE
+const status = {description:"tell me about this repo", code:"git status"};
+//////////////////PROBLEM 6////////////////////
+/*
+    Create an object called 'add' with 'description' and 'code' properties 
+    following the guidelines above to describe the add command.
+*/
+
+//CODE HERE
+const add = {description:"add some files to be tracked", code:"git add"};
+//////////////////PROBLEM 7////////////////////
+/*
+    Create an object called 'commit' with 'description' and 'code' properties
+    following the guidelines above to describe the commit command.
+*/
+
+//CODE HERE
+const commit = {description:"create a snapshot of the files I’m tracking", code:"git commit"};
+
+//////////////////PROBLEM 8////////////////////
+/*
+    Create an object called 'push' with 'description' and 'code' properties 
+    following the guidelines above to describe the push command.
+*/
+
+//CODE HERE
+const push ={description:"upload local repsitory content to a remote repository", code:"git push <remote> <branch>"};
---
+++ sldc.js
@@ -0,0 +1,33 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Analysis of Requirements
+// Design
+// Implementation
+// Maintain
+// Planning
+// Testing & Integration
+
+// answer to Problem 1:
+// Planning
+// Analysis of Requirements
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of
+     each variable to a definition of the step.
+*/
+var planning = ("the phase in which developers will plan for the upcoming project")
+var analysisofRequirements = ("Gathering all the specfic details required for a new system as well as determing the first ideas for prototypes")
+var design = ("outline the details for the overall application, alongside specific aspects")
+var implementation = ("develop the code and build the application according to the earlier design documents and outlined specifications")
+var testingAndIntegration = ("with pre-planned scenarios as part of software design and coding and conducting performance testing to simulate load testing on the application")
+var maintain = ("begin practicing any activities required to handle issues reported by end-users")
